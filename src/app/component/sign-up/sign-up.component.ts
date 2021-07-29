2234ff836e637cdc9b353fd99393b11d71982e3f
kent5i5 <oceanamng@hotmail.com>
2021-07-29 13:39:18 -0700

    finish fixing the signup method

--- src/app/component/sign-up/sign-up.component.ts
+++ src/app/component/sign-up/sign-up.component.ts
@@ -1,13 +1,12 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
-
-import { AuthService } from 'src/app/service/auth.service';
 
 import { Observable } from 'rxjs';
 import { Role } from 'src/app/models/Role';
 import { User } from 'src/app/models/User';
 import { AuthService } from 'src/app/service/auth.service';
-import { UserService } from 'src/app/service/user.service';
+
 
 
 @Component({
@@ -16,37 +15,30 @@ import { UserService } from 'src/app/service/user.service';
   styleUrls: ['./sign-up.component.css']
 })
 export class SignUpComponent implements OnInit {
-
+  users: User[] = [];
 
    //tried to inject the serivce
-  constructor(private authservice : AuthService) { }
-
-  role: Role = {
-   role_id: 1,
-   role: "employye"
- };
+  constructor(private authservice : AuthService, public http : HttpClient) { }
+     
+  // all new user is client
+    role: Role = {
+    role_id: 1,
+    role: "employye"
+  };
   user: User = {
-    userId: 1,
+   userId: null,
     username: 'registerkenny',
     password: 'pwd',
     first_name: 'ken',
     last_name: 'eng',
-    role_id:  this.role,
-    recipe_id: 1
+    role_id:  this.role
   };
 
-  retUser : Observable<User>
-  constructor(userService: UserService, authService : AuthService) {
-       
-      
-      this.retUser  = authService.register(this.user);
-
-  }
-
-
+  userRef : Observable<User> | undefined
+  
   ngOnInit(): void {
+     
   }
-
 
  //tried to do somthing simlar what I did in the login but with registar method that is in auth.service
   onSubmit(f: NgForm) {
@@ -57,7 +49,23 @@ const registerObserver = {
 
 };
 //tried to to the login dont think this is correct
-    //this.authservice.register(f.value).subscribe(registerObserver);
+      
+      // update the user with the data from the form
+      this.user.username = f.value.username;
+      this.user.first_name = f.value.first_name;
+      this.user.last_name = f.value.last_name;
+      this.user.password = f.value.password;
+      this.authservice.register(this.user)
+      .subscribe(newUser => {
+        this.users.push(newUser);
+      });
+  
+  
+      
+    // this.http.post<any>('http://localhost:8088/boot/users/register', )
+    // .subscribe(
+    //   data => localStorage.setItem("username", data.username);
+    //  );
 
     console.log(f.value);  // { first: '', last: '' }
     console.log(f.valid);  // false
